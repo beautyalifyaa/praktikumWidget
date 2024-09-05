77d01e398b65f32562dab401d26abe84e0cb020c
beautyalifyaa <141795694+beautyalifyaa@users.noreply.github.com>
2024-09-05 07:29:23 +0700

    Add files via upload

---
+++ list_view_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ListViewWidget extends StatelessWidget {
+  const ListViewWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+    debug
+  }
+}
